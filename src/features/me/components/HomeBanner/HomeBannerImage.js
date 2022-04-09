9750090678e2e54f7222e0526b49fad635fa1f81
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2022-04-09 14:32:08 -0300

    default card

--- src/features/me/components/HomeBanner/HomeBannerImage.js
+++ src/features/me/components/HomeBanner/HomeBannerImage.js
@@ -7,13 +7,14 @@ import { Link, withTranslation } from "@i18n";
 
 import { Carousel } from "react-responsive-carousel";
 import authors from "@/constants/authors";
+import images from "@/constants/images";
 
 const HomeBannerImage = ({ className, t, destaks = [] }) => {
     return (
         <Carousel className={className} autoPlay infiniteLoop showArrows showStatus={false} showThumbs={false}>
             {destaks.filter(d => d.title).map(d => (
                 <a key={`${d.title}${d.author}`} href={d.route}>
-                    <div className="carousel-item" style={{ backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 21, 68, 0) 60%), url("${d.img}")`}} />
+                    <div className="carousel-item" style={{ backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 21, 68, 0) 60%), url("${d.img ? d.img : images.defaultCard}")`}} />
                     <p className="legend">
                         {d.title}
                         <p className="subtitle">Por {d.author ? d.author : authors[d.category].name}</p>
