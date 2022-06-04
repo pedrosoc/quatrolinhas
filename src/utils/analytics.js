220905ad30da6774e3c28d6b44564c605577441c
Lucas Pedroso <lucas.pedroso@dextra-sw.com>
2022-06-04 17:18:13 -0300

    feat: adding ga

--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -4,7 +4,7 @@ import ReactGA from 'react-ga'
 
 export const initGA = () => {
     console.log('GA init')
-    ReactGA.initialize('UA-QUATRO LINHAS')
+    ReactGA.initialize('UA-156577109-1')
 }
 
 export const logPageView = () => {
